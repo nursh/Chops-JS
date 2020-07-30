9abaefd30b0188be4949c15aa153b9efcb21227c
nursh <nsheikhmohammed01@gmail.com>
2020-07-29 20:12:51 -0400

    Add configuration for eslint and prettier.

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,19 +1,16 @@
-import express, {Application} from "express";
-import {ApolloServer} from 'apollo-server-express';
-import {typeDefs, resolvers} from './graphql';
+import express from "express";
+import { ApolloServer } from "apollo-server-express";
+import { typeDefs, resolvers } from "./graphql";
 
 const app = express();
 app.use(express.json());
-app.use(express.urlencoded({extended: false}));
+app.use(express.urlencoded({ extended: false }));
 const port = process.env.PORT || 8080;
 
 const server = new ApolloServer({
-  typeDefs, resolvers
+    typeDefs,
+    resolvers,
 });
 
-server.applyMiddleware({ app, path: '/api'});
+server.applyMiddleware({ app, path: "/api" });
 app.listen(port, () => console.log(`[app]: running on port ${port}`));
-
-
-
-
